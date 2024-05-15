425ecc41915a0abf05596623aa471df509c99901
Jungwon_Park <Jungwon.Park@edu.sait.ca>
2024-05-14 20:10:33 -0600

    Update week2-assginment
    Following part 2 directions, completed the task

--- app/week-2/page.js
+++ app/week-2/page.js
@@ -1,7 +1,10 @@
+import StudentInfo from "./student-info";
+
 export default function Page() {
   return (
-    <main>
+    <main className="h-screen">
       <h1>Shopping List</h1>
+      <StudentInfo />
     </main>
   );
 }
---
+++ app/week-2/student-info.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+  return (
+    <div>
+      <h1>Jungwon(Noah) Park</h1>
+      <Link href="https://github.com/NoahPark9001?tab=repositories">
+        https://github.com
+      </Link>
+    </div>
+  );
+}
