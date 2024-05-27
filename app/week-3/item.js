c7efa5a33c3e69cfc8e9df439791bbe45e76dd7a
Jungwon_Park <Jungwon.Park@edu.sait.ca>
2024-05-27 00:34:41 -0600

    Update CSS
    Update CSS page.js and item.js in week-3 folder

--- app/week-3/item.js
+++ app/week-3/item.js
@@ -1,8 +1,8 @@
 export default function Item({ name, quantity, category }) {
   return (
-    <div>
-      <h2 className="text-2xl font-bold"> {name}</h2>
-      <p className="text-sm">
+    <div className="p-2 m-4  border-red-500 rounded-lg bg-stone-700 max-w-sm">
+      <h2 className="text-2xl font-bold text-white"> {name}</h2>
+      <p className="text-sm text-stone-300">
         Buy {quantity} In {category}
       </p>
     </div>
--- app/week-3/page.js
+++ app/week-3/page.js
@@ -3,8 +3,10 @@ import ItemList from "./item-list";
 
 export default function Page() {
   return (
-    <main>
-      <h1 className="text-3xl font-bold">Shopping list</h1>
+    <main className="bg-[#656d4a]">
+      <h1 className="text-3xl text-[#252422] font-bold text-justified text-justified m-1">
+        Shopping List
+      </h1>
       <ul>
         <ItemList />
       </ul>
